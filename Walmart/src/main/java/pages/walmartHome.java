c1bc9503ee7d59e9c92dca495561c69a2ae367df
Selina Tanha <tanha.selina75@gmail.com>
2018-05-10 22:02:25 -0400

    thursday commit s

--- Walmart/src/main/java/pages/walmartHome.java
+++ Walmart/src/main/java/pages/walmartHome.java
@@ -12,5 +12,6 @@ public class walmartHome extends MobileAPI{
         ad.findElementByXPath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.support.v4.widget.DrawerLayout/android.widget.FrameLayout/android.view.View/android.support.v7.widget.LinearLayoutCompat/android.widget.LinearLayout/android.widget.LinearLayout/android.widget.LinearLayout/android.widget.EditText").click();
        // ad.findElementByXPath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.support.v4.widget.DrawerLayout/android.widget.FrameLayout/android.view.View/android.support.v7.widget.LinearLayoutCompat/android.widget.LinearLayout/android.widget.LinearLayout/android.widget.LinearLayout/android.widget.EditText").sendKeys("laptop", Keys.ENTER);
         ad.findElementByXPath("//android.widget.TextView[@content-desc=\"Free 2-Day Shipping button\"]").click();
+        //why???
     }
 }
